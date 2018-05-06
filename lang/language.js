8491c76a5ecb7488f8c86deb01f09039ee63a4df
mutoe <mutoe@foxmail.com>
2018-05-06 12:01:04 +0800

    i18n support

---
+++ lang/language.js
@@ -0,0 +1,11 @@
+$.i18n.lang = $.extend(true, $.i18n.lang || {}, {
+  en: {
+    title: 'ONI Geyser Calculator (Preview)',
+  },
+  zh: {
+    title: '缺氧 间歇泉计算器（预览版）',
+  },
+  'zh-tw': {
+    title: '缺氧 間歇泉計算器',
+  },
+})
