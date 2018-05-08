6b371903ddb295d3274aba405676c6557d0afd49
mutoe <mutoe@foxmail.com>
2018-05-08 11:41:42 +0800

    automation API

---
+++ api/api.js
@@ -0,0 +1,66 @@
+const axios = require('axios')
+const qs = require('querystring')
+
+/**
+ * Baidu AI OCR API
+ * http://ai.baidu.com/docs#/OCR-API/top
+ */
+class API {
+  static async getAccessToken () {
+    const params = {
+      'grant_type': 'client_credentials',
+      'client_id': process.env.CLIENT_ID,
+      'client_secret': process.env.CLIENT_SECRET,
+    }
+    return axios.get('https://aip.baidubce.com/oauth/2.0/token', { params })
+      .then(res => res.data.access_token)
+      .catch(err => { throw new Error(err) })
+  }
+
+  static async ocr (token, image) {
+    const url = `https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=${token}`
+    const data = {
+      image,
+      language_type: 'CHN_ENG',
+    }
+    return axios.post(url, qs.stringify(data))
+      .then(res => res.data.words_result)
+      .catch(err => err)
+  }
+
+  static analyze (ocrResult) {
+    const ret = {
+      type: '',
+      temp: 0,
+      output: 0,
+      ep: [],
+      ap: [],
+    }
+    for (let i of ocrResult) {
+      let word = i.words
+      if (word.length < 6) continue
+      let match
+      // 识别温泉种类和喷发量
+      if ((match = word.match(/^(\S+)[;:]\S*?([.\d]+)(千?)克\/秒\S*/))) {
+        const tempMatch = match[0].match(/(-?[.\d]+?)摄氏度/)
+        ret.type = match[1]
+        ret.temp = Number(tempMatch[1])
+        ret.output = match[2] * (match[3] ? 1000 : 1)
+        continue
+      }
+      // 识别喷发期
+      if ((match = word.match(/喷发\S+?[;:]\S*?(\d+)秒\S+?(\d+)秒/))) {
+        ret.ep = [Number(match[1]), Number(match[2])]
+        continue
+      }
+      // 识别活跃期
+      if ((match = word.match(/活(?:跃期|动周期)[;:]\S*?([.\d]+)(?:个周期|天)\S*?([.\d]+)(?:个周期|天)/))) {
+        ret.ap = [Number(match[1]), Number(match[2])]
+        continue
+      }
+    }
+    return ret
+  }
+}
+
+module.exports = API
--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Koa = require('koa')
 const path = require('path')
 const views = require('koa-views')
@@ -7,12 +8,18 @@ const BodyParser = require('koa-bodyparser')
 const router = require('./router')
 
 const app = new Koa()
+const port = process.env.PORT || 3000
+
 app.use(Logger())
-app.use(BodyParser())
+
+app.use(BodyParser({
+  formLimit: '1mb',
+}))
+
 app.use(views(path.join(__dirname, 'views'), { extension: 'pug' }))
 
 app.use(router.routes())
 
-app.listen(3000, () => {
-  console.log('koa is listening in port 3000...')
+app.listen(port, () => {
+  console.log(`koa is listening in port ${port}...`)
 })
